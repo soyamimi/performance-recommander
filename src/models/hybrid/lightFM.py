b2e8a76f86be2bc6a47bd0cbd748d2f3e8e826b6
Seohyun Park <seohypark@gmail.com>
2025-10-11 15:20:10 +0200

    style: run black 🎨

--- src/models/hybrid/lightFM.py
+++ src/models/hybrid/lightFM.py
@@ -11,6 +11,7 @@ from utils.helpers import read_csv_to_dataframe
 
 BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), "../../.."))
 
+
 class LightFMRecommender:
     def __init__(self, interactions, item_features, loss, epochs, num_threads):
         self.interactions = interactions
@@ -20,25 +21,33 @@ class LightFMRecommender:
         self.num_threads = num_threads
 
     def train(self):
-        self.model.fit(self.interactions, item_features=self.item_features,
-                       epochs=self.epochs, num_threads=self.num_threads)
+        self.model.fit(
+            self.interactions,
+            item_features=self.item_features,
+            epochs=self.epochs,
+            num_threads=self.num_threads,
+        )
 
     def evaluate(self, k):
-        precision = precision_at_k(self.model, self.interactions,
-                                   item_features=self.item_features, k=k).mean()
-        auc = auc_score(self.model, self.interactions,
-                        item_features=self.item_features).mean()
+        precision = precision_at_k(
+            self.model, self.interactions, item_features=self.item_features, k=k
+        ).mean()
+        auc = auc_score(
+            self.model, self.interactions, item_features=self.item_features
+        ).mean()
         return precision, auc
 
     def recommend(self, user_id):
         n_items = self.interactions.shape[1]
-        scores = self.model.predict(user_id, np.arange(n_items), item_features=self.item_features)
+        scores = self.model.predict(
+            user_id, np.arange(n_items), item_features=self.item_features
+        )
         top_items = np.argsort(-scores)
         return top_items, scores[top_items]
 
 
 if __name__ == "__main__":
-    user_data= read_csv_to_dataframe(
+    user_data = read_csv_to_dataframe(
         BASE_DIR / Path(settings.RAW_DATA_FOLDER) / "users_2025.csv"
     ).to_numpy()
     interactions = csr_matrix(user_data)
@@ -47,7 +56,9 @@ if __name__ == "__main__":
         BASE_DIR / Path(settings.PREPROCESSED_DATA_FOLDER) / "preprocessed_data.npz"
     )
 
-    recommender = LightFMRecommender(interactions, item_features=item_features, epochs=30,loss='warp', num_threads=2)
+    recommender = LightFMRecommender(
+        interactions, item_features=item_features, epochs=30, loss="warp", num_threads=2
+    )
 
     print("train...")
     recommender.train()
